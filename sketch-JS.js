8d4b5644a63f7a3435e99a9aade50110b0d0a47b
Kaitlyn Hardin <khardin9@my.apsu.edu>
2022-05-11 18:00:33 -0500

    Change files to fit CSS Flexbox.

--- sketch-JS.js
+++ sketch-JS.js
@@ -1,12 +1,9 @@
-const container = document.getElementById("grid-container");
+const container = document.getElementById("flex-container");
 
 function makeRows(rows, cols) {
-  container.style.setProperty('--grid-rows', rows);
-  container.style.setProperty('--grid-cols', cols);
   for (c = 0; c < (rows * cols); c++) {
     let cell = document.createElement("div");
-    cell.innerText = (c + 1);
-    container.appendChild(cell).className = "grid-item";
+    container.appendChild(cell).className = "flex-item";
   }
 };
 
