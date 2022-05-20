73c432ee6931682d51087337e47a59cb09f089e0
Kaitlyn Hardin <khardin9@my.apsu.edu>
2022-05-20 17:11:58 -0500

    Add hover styling to boxes using CSS and event handlers.

--- sketch-JS.js
+++ sketch-JS.js
@@ -1,4 +1,5 @@
 const container = document.getElementById("flex-container");
+let gridSize = 16;
 
 /**
  * This function make a grid using the rows and columns. 
@@ -6,12 +7,53 @@ const container = document.getElementById("flex-container");
  * @param {This parameter represents the amount of rows in the grid} rows 
  * @param {This parameter represents the amount of columns in the grid} cols 
  */
-function makeGrid(rows, cols) {
-  for (c = 0; c < (rows * cols); c++) {
-    let cell = document.createElement("div");
-    container.appendChild(cell).className = "flex-item";
+function makeGrid(rows, columns) {
+  for (c = 0; c < rows; c++) {
+    for (let d = 0; d < columns; d++) {
+      let cell = document.createElement("div");
+      cell.innerText = (d + 1) % columns;
+      container.appendChild(cell).className = "flex-item";
+    }
   }
 };
+makeGrid(gridSize, gridSize);
 
 
-makeGrid(16, 16);
+//Adds event when the mouse over a flex item 
+//and applies hover class styling in CSS
+container.addEventListener("mouseover", function (e) {
+  if (e.target.matches('.flex-item')) {
+    e.target.classList.add('hover');
+  }
+});
+
+//Adds event when the mouse clicks a flex item 
+//and removes hover class styling in CSS
+container.addEventListener("click", function (e) {
+  if (e.target.matches('.flex-item')) {
+    e.target.classList.remove('hover');
+  }
+});
+
+function removeAllChildNodes(parent) {
+  while (parent.firstChild) {
+    parent.removeChild(parent.firstChild)
+  }
+}
+
+
+let button = document.createElement("button");
+button.setAttribute(
+  "style",
+  "height: 50px; width: 50px; text-align: center; "
+);
+
+
+button.addEventListener("click", function () {
+  gridSize = parseInt(prompt("Grid Size: "));
+  removeAllChildNodes(container);
+  makeGrid(gridSize, gridSize);
+});
+
+document.body.appendChild(button);
+
