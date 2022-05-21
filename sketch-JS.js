29aeed9ef9113541194e42e0b72f883723ece59f
Kaitlyn Hardin <khardin9@my.apsu.edu>
2022-05-21 15:51:59 -0500

    Change to GRID layout instead of Flexbox
    This layout change was chosen because a 2D layout is
    easier to create using GRID rather than flexbox.

--- sketch-JS.js
+++ sketch-JS.js
@@ -1,28 +1,31 @@
-const container = document.getElementById("flex-container");
+const container = document.getElementById("grid-container");
 let gridSize = 16;
 
 /**
  * This function make a grid using the rows and columns. 
- * A div element is created and appended to the flex container.
+ * A div element is created and appended to the grid container.
  * @param {This parameter represents the amount of rows in the grid} rows 
  * @param {This parameter represents the amount of columns in the grid} cols 
  */
-function makeGrid(rows, columns) {
-  for (c = 0; c < rows; c++) {
-    for (let d = 0; d < columns; d++) {
-      let cell = document.createElement("div");
-      cell.innerText = (d + 1) % columns;
-      container.appendChild(cell).className = "flex-item";
-    }
+function makeGrid(gridSize) {
+  let totalDivs = gridSize * gridSize;
+
+  container.style.gridTemplateColumns = `repeat(${gridSize}, 50px)`;
+  container.style.gridTemplateRows = `repeat(${gridSize}, 50px)`;
+
+  for (let i = 0; i < totalDivs; i++) {
+    let cell = document.createElement("div");
+    // cell.innerText = (i + 1) % gridSize;
+    container.appendChild(cell).className = "grid-item";
   }
 };
-makeGrid(gridSize, gridSize);
+makeGrid(gridSize);
 
 
 //Adds event when the mouse over a flex item 
 //and applies hover class styling in CSS
 container.addEventListener("mouseover", function (e) {
-  if (e.target.matches('.flex-item')) {
+  if (e.target.matches('.grid-item')) {
     e.target.classList.add('hover');
   }
 });
@@ -30,7 +33,7 @@ container.addEventListener("mouseover", function (e) {
 //Adds event when the mouse clicks a flex item 
 //and removes hover class styling in CSS
 container.addEventListener("click", function (e) {
-  if (e.target.matches('.flex-item')) {
+  if (e.target.matches('.grid-item')) {
     e.target.classList.remove('hover');
   }
 });
